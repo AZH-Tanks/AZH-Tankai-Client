4ea6cb2c08a9ea1fbfcf1108d51cb6dbf3e155dc
pockeviciuspovilas <47566999+pockeviciuspovilas@users.noreply.github.com>
2020-09-16 22:14:04 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace signalrClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OutputBox = new System.Windows.Forms.RichTextBox();
+            this.username = new System.Windows.Forms.TextBox();
+            this.CreatePlayerButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.BackColor = System.Drawing.Color.White;
+            this.OutputBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.OutputBox.Location = new System.Drawing.Point(12, 65);
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.ReadOnly = true;
+            this.OutputBox.Size = new System.Drawing.Size(401, 521);
+            this.OutputBox.TabIndex = 8;
+            this.OutputBox.Text = "";
+            // 
+            // username
+            // 
+            this.username.BackColor = System.Drawing.Color.White;
+            this.username.Location = new System.Drawing.Point(12, 27);
+            this.username.Name = "username";
+            this.username.PlaceholderText = "Name..";
+            this.username.Size = new System.Drawing.Size(308, 27);
+            this.username.TabIndex = 10;
+            // 
+            // CreatePlayerButton
+            // 
+            this.CreatePlayerButton.Location = new System.Drawing.Point(326, 27);
+            this.CreatePlayerButton.Name = "CreatePlayerButton";
+            this.CreatePlayerButton.Size = new System.Drawing.Size(87, 27);
+            this.CreatePlayerButton.TabIndex = 11;
+            this.CreatePlayerButton.Text = "Submit";
+            this.CreatePlayerButton.UseVisualStyleBackColor = true;
+            this.CreatePlayerButton.Click += new System.EventHandler(this.CreatePlayerButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 600);
+            this.Controls.Add(this.CreatePlayerButton);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.OutputBox);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox OutputBox;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.Button CreatePlayerButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace signalrClient
+{
+    public partial class Form1 : Form
+    {
+        const int speed = 5;
+        string currentUser = null;
+        HubConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.KeyDown += Form1_KeyDown;
+            this.KeyPreview = true;
+
+            connection = new HubConnectionBuilder()
+              .WithUrl("https://localhost:44308/ControlHub")
+              .Build();
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+                OutputBox.Text += error.Message + "\n";
+            };
+
+        }
+
+        Button tank = new Button();
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            OutputBox.Text += "Starting connection..\n";
+            await connection.StartAsync();
+            OutputBox.Text += "Connection started!\n";
+
+            connection.On<string, int, int>("ReceiveCoordinate", (user, x, y) =>
+            {
+                this.BeginInvoke((Action)(() =>
+                {
+                    tank.Location = new Point(x, y);
+                }));
+            });
+
+            connection.On<string>("ReceiveUser", (user) =>
+            {
+                this.BeginInvoke((Action)(() =>
+                {
+                    currentUser = user;
+                    OutputBox.Text += $"{user} joined!\n";
+                    tank.BackColor = Color.FromArgb(new Random().Next(1, 255), new Random().Next(1, 255), new Random().Next(1, 255));
+                    tank.Width = 30;
+                    tank.Height = 30;
+                    tank.Location = new Point(500, 200);
+                    tank.Enabled = false;
+                    this.Controls.Add(tank);
+                }));
+            });
+        }
+
+        private async void CreatePlayerButton_Click(object sender, EventArgs e)
+        {
+            await connection.InvokeAsync("SendPlayer", username.Text);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (currentUser != null)
+            {
+                int x = tank.Location.X;
+                int y = tank.Location.Y;
+
+                if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+                {
+                    x += speed;
+                }
+                else if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+                {
+                    x -= speed;
+                }
+                else if (e.KeyCode == Keys.W || e.KeyCode == Keys.Up)
+                {
+                    y -= speed;
+                }
+                else if (e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
+                {
+                    y += speed;
+                }
+
+                if (tank.Location.X != x || tank.Location.Y != y)
+                {
+                    tank.Location = new Point(x, y);
+                    OutputBox.Text += $"X:{x}, Y:{y}\n";
+                    connection.InvokeAsync("SendCoordinate", username.Text, x, y);
+                }
+            }
+        }
+
+
+    }
+
+}
